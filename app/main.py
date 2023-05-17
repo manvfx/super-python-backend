1106aa1b22150111694f3ec6a33e5b786f1af2fd
manvfx <manvfx@gmail.com>
2023-05-17 11:26:04 +0330

    update main file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
 from app.database import create_user, get_users
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(
+    title="Super Backend",
+    version="1.0.0",
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {
+        "message": "Welcome to this app!",
+    }
 
 @app.post("/users")
 def create_user_endpoint(user_data: dict):
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+# main.py
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000,reload=True,log_level="info")
